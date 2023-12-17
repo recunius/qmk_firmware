21eb129b2b9739c6a2d25bb3ed6faf60dbca7fdd
Robert Cunius Jr <recunius@espoc.com>
2023-12-16 22:13:04 -0500

    updated colors and gaming esc key position/layer

--- keyboards/keychron/v2/ansi_encoder/keymaps/recunius/keymap.c
+++ keyboards/keychron/v2/ansi_encoder/keymaps/recunius/keymap.c
@@ -54,16 +54,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN_GAME0] = LAYOUT_ansi_67(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_PGUP,
-        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_ESC,
+        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_PGDN,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,
         LT(_FN_GAME1, KC_SLSH), KC_BSLS,  KC_LALT,                             KC_SPC,                             KC_RALT,  _______,  _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN_GAME1] = LAYOUT_ansi_67(
-        KC_GRV, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_DEL,          _______,
+        KC_ESC, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_DEL,          _______,
         _______, _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
         _______, _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,          _______,
         _______,           _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______, _______,
-        _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
+        _______, KC_LWIN,  _______,                             KC_ESC,                            _______,  _______,  _______,  _______, _______, _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -84,16 +84,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     for(int i=0; i < sizeof(layer_leds) / sizeof(uint8_t); i++) {
         switch(get_highest_layer(layer_state|default_layer_state)) {
             case _FN0:
-                rgb_matrix_set_color(layer_leds[i], RGB_GREEN);
+                rgb_matrix_set_color(layer_leds[i], RGB_CORAL);
                 break;
             case _FN1:
-                rgb_matrix_set_color(layer_leds[i], RGB_ORANGE);
+                rgb_matrix_set_color(layer_leds[i], RGB_GREEN);
                 break;
             case _FN_GAME0:
-                rgb_matrix_set_color(layer_leds[i], RGB_BLUE);
+                rgb_matrix_set_color(layer_leds[i], RGB_MAGENTA);
                 break;
             case _FN_GAME1:
-                rgb_matrix_set_color(layer_leds[i], RGB_MAGENTA);
+                rgb_matrix_set_color(layer_leds[i], RGB_TURQUOISE);
                 break;
             default:
                 rgb_matrix_set_color(layer_leds[i], RGB_OFF);
